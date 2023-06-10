258e8dd6cbe666e14f6f8cb6721c74f216b58023
01one <84787013+01one@users.noreply.github.com>
2023-06-10 22:38:11 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const cacheName = 'purnorup-cache';
+const filesToCache = [
+  '/',
+  'background.png',
+  'logo.png',
+  'logo_icon.ico',
+  'case converter.html',
+  'image cropper.html',
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
