e97323a7b273d56dce46990aafa450e2d63da06a
01one <84787013+01one@users.noreply.github.com>
2023-06-20 01:24:20 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,37 @@
-const cacheName = 'purnorup-cache-v2.2';
+const cacheName = 'purnorup-cache-v2.3';
 const filesToCache = [
-  '/',
   'logo.png',
   'logo_icon.ico',
 ];
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
+    caches.open(cacheName)
+      .then(function(cache) {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
     caches.keys().then(function(cacheNames) {
       return Promise.all(
-        cacheNames.filter(function(cache) {
-          return cache.startsWith('purnorup-cache-v2') || cache.startsWith('purnorup-cache-v2.1') && cache !== cacheName;
-        }).map(function(cache) {
-          return caches.delete(cache);
+        cacheNames.map(function(cache) {
+          if (cache !== cacheName) {
+            return caches.delete(cache);
+          }
         })
       );
-    }).then(function() {
-      return caches.open(cacheName);
-    }).then(function(cache) {
-      return cache.addAll(filesToCache);
     })
   );
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
   );
 });
